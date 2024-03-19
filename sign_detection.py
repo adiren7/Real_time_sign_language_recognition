2cd1e09b80c249d53449cac1676b954f754a1363
adiren7 <adiloubaibou2001@gmail.com>
2024-03-19 01:05:30 +0000

    sign detection

--- sign_detection.py
+++ sign_detection.py
@@ -13,11 +13,11 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(staticu_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 labels_dict = {
-    0: 'A', 1: 'B', 2: 'C', 3: 'C', 4: 'del', 5: 'E', 6: 'F', 7: 'G', 8: 'del', 9: 'I', 10: 'J', 11: 'K',
-    12: 'L', 13: 'A', 14: 'N', 15: 'nothing', 16: 'C', 17: 'P', 18: 'Q', 19: 'R', 20: 'A', 21: 'space',
+    0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'del', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K',
+    12: 'L', 13: 'M', 14: 'N', 15: 'nothing', 16: 'O', 17: 'P', 18: 'Q', 19: 'R', 20: 'S', 21: 'space',
     22: 'T', 23: 'U', 24: 'V', 25: 'W', 26: 'X', 27: 'Y', 28: 'Z'
 }
 
